a9388b5f6c470903f379b4326a239728de6f4a7e
UnknownGroup2023 <unknownreact2023@gmail.com>
2025-02-03 11:08:25 +0330

    title payment

---
+++ src/app/Payment/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+  return (
+    <div className="  w-full min-h-screen bg-gradient-to-b from-[#252D62] via-[#343F89] to-[#4C5CC8]">
+      <div className=" relative flex justify-center items-center min-h-screen top-[-144px]">
+        <img
+          src={"/images/payment.png"}
+          alt="Logo"
+          className="object-cover w-[695.1px] h-[85.63px] max-w-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default page;
