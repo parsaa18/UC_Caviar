9092efd9c2cdc26f957f0c05359db86321d36811
UnknownGroup2023 <unknownreact2023@gmail.com>
2025-02-07 14:09:45 +0330

    prouduct detail

---
+++ src/app/products/[category]/[id]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <div className="w-[45px] h-[19px] text-[16px] leading-[19.2px] text-[#00000080] opacity-70 mx-auto mt-[123px]">
+        Caviar
+      </div>
+      <div className="w-[420px] h-[58px] text-[48px] leading-[57.6px] text-black  mx-auto mt-[2px]">
+        Beluga Imperial Top
+      </div>
+      <div className=" flex  justify-center gap-[2.37px] w-[57px] h-[22.6px] text-[16px] leading-[16.8px] text-[#000000]">
+        Back
+        <img src="" />
+      </div>
+    </>
+  );
+}
+
+export default page;
