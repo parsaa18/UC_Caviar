65f41922861b9c5fdb1206ed26255cbaecd9de48
UnknownGroup2023 <unknownreact2023@gmail.com>
2025-02-11 15:25:15 +0330

    course detail responsive

--- src/app/products/[category]/[id]/page.tsx
+++ src/app/products/[category]/[id]/page.tsx
@@ -1,88 +1,133 @@
 import DoubleBtn from "@/components/common/DoubleButton";
 import Rate from "@/components/common/Rate";
 import Slider from "@/components/common/Slider";
+import Image from "next/image";
 
 import React from "react";
 
 function page() {
   return (
     <>
-      <div className="w-[45px] h-[19px] text-[16px] leading-[19.2px] text-[#000000] opacity-50 mx-auto mt-[123px]">
-        Caviar
-      </div>
-      <div className="flex justify-center gap-[91px] w-[50S0px] h-[58px] text-[40px] leading-[57.6px] text-black ml-[-150px] mx-auto mt-[2px] sm:text-[48px] sm:gap-[103px]">
-        <div className="flex justify-center gap-[2.37px] w-[57px] h-[22.6px] text-[16px] leading-[16.8px] text-[#000000] opacity-50 mt-[500px] sm:mt-[16px]">
-          <img
-            src={"/images/arrow-right.png"}
-            alt="Logo"
-            className="object-cover w-[36px] h-[30px] mt-[-5px] max-w-full"
+      <div className="flex items-center justify-center md:mx-auto ml-[-120px] gap-[80px] md:gap-[40px] mt-[123px] md:mt-[123px] ">
+        <button className="flex items-center space-x-1 text-gray-500 hover:text-gray-700 transition md:hidden">
+          <Image
+            src="/images/arrow-right.png"
+            alt="Arrow"
+            width={26}
+            height={26}
           />
-          Back
+          <span className="text-[16px]">Back</span>
+        </button>
+
+        <div className="w-[45px] h-[19px] text-[16px] leading-[19.2px] text-[#000000] opacity-50 text-center">
+          Caviar
         </div>
-        Beluga Imperial Top
       </div>
 
-      <div className="h-[450px]">
-        <div className="mt-[48px]">
+      <div className="flex flex-wrap items-center md:ml-[-150px]  justify-center px-6 md:px-12 py-4 gap-[20px] md:gap-x-[103px]">
+        <button className="hidden md:flex items-center space-x-1 text-gray-500 hover:text-gray-700 transition">
+          <Image
+            src="/images/arrow-right.png"
+            alt="Arrow"
+            width={26}
+            height={26}
+          />
+          <span className="text-[16px]">Back</span>
+        </button>
+
+        <div className="text-[40px]  md:text-[48px]  text-[#0D0A0B] text-center md:text-center">
+          Beluga Imperial Top
+        </div>
+      </div>
+
+      <div className=" flex flex-col  md:flex md:flex-wrap justify-center items-start  md:items-center mx-auto md:ml-[150px] md:gap-[6px] space-x-0  h-[330px]">
+        <div className="  hidden md:flex md:flex-col gap-[24px] w-[200px] ">
+          <div className=" flex flex-col gap-[12px] w-[200px] ">
+            <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold ">
+              Order Now
+            </div>
+            <div className="">
+              <DoubleBtn theme="blue" text="Contact Us" />
+            </div>
+          </div>
+          <div className=" flex flex-col gap-[12px] w-[250px] ">
+            <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]  ">
+              How we send products?
+            </div>
+            <div className="">
+              <DoubleBtn theme="blue" text="Shipping" />
+            </div>
+          </div>
+        </div>
+        <div className=" w-[450px]  md:mt-[8px] mt-[260px] ">
           <Slider />
         </div>
-        <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold mt-[-206px] ml-[332px] mr-[140px]">
-          Order Now
-        </div>
-        <div className="ml-[322px] mt-[12px]">
-          <DoubleBtn theme="blue" text="Contact Us" />
-        </div>
-        <div className="w-[177px] h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B] mt-[24px] ml-[322px]">
-          How we send products?
-        </div>
-        <div className="ml-[322px] mt-[12px]">
-          <DoubleBtn theme="blue" text="Shipping" />
-        </div>
-        <div className="flex justify-center gap-[48px] w-[180px] h-[29px] mt-[-171px] ml-[927px]">
-          <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
-            Color
-          </div>
-          <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            Dark Gray
-          </div>
-        </div>
 
-        <div className="flex justify-center gap-[48px] w-[180px] h-[29px] mt-[10px] ml-[927px]">
-          <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
-            Size
+        <div className=" w-[350px]">
+          <div className=" md:mt-[0] mt-[130px] md:ml-0 ml-[32px]  justify-start  flex  flex-wrap gap-[145px] md:gap-[48px]">
+            <div className=" flex flex-col md:gap-[16px] gap-[8px] ">
+              <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
+                Color
+              </div>
+              <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
+                Size
+              </div>
+              <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
+                Rate
+              </div>
+            </div>
+            <div className=" flex flex-col md:gap-[16px] gap-[8px] ">
+              <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
+                Dark Gray
+              </div>
+              <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
+                3.3 ≤ mm Up
+              </div>
+              <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
+                <Rate />
+              </div>
+            </div>
           </div>
-          <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            3.3 ≤ mm Up
+          <div className="text-[16px] mt-[26px] leading-[19.2px] w-full md:ml-0 ml-[32px] h-[57px] text-[#000000] ">
+            A unique Korean flavor with a special sea taste, a silky smooth and
+            velvety texture, and a distinctive salty taste
           </div>
         </div>
-
-        <div className="flex justify-center gap-[48px] w-[180px] h-[29px] mt-[10px] ml-[927px]">
-          <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
-            Rate
-          </div>
-          <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            <Rate />
-          </div>
-        </div>
-        <div className="text-[16px] leading-[19.2px] w-[285px] h-[57px] text-[#000000] mt-[10px] ml-[927px]">
-          A unique Korean flavor with a special sea taste, a silky smooth and
-          velvety texture, and a distinctive salty taste
-        </div>
-
-        <div className="flex justify-center gap-[96px] w-[180px] h-[29px] mt-[59px] ml-[580px]">
+      </div>
+      <div className="flex flex-wrap gap-[76px] md:gap-[86px] md:ml-0 ml-[32px] md:justify-center justify-start items-center mt-[23px]">
+        <div className="flex flex-col gap-[8px]">
           <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
             Weight
           </div>
-          <div className="w-[49px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            50 g
-          </div>
-        </div>
-        <div className="flex justify-center gap-[24px] w-[280px] h-[29px] mt-[8px] ml-[545px]">
           <div className="text-[16px] leading-[19.2px] w-[111px] h-[19px] text-[#000000] opacity-50">
             Packaging Type
           </div>
+        </div>
+
+        <div className="flex flex-col gap-[8px]">
+          <div className="w-[49px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
+            50 g
+          </div>
           <div className="w-[74px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
             Desjardins
+          </div>
+        </div>
+        <div className="  md:hidden flex justify-center items-center mt-[72px]  gap-[24px] w-[400px] h-[200px] ">
+          <div className=" flex flex-col gap-[12px] w-[250px] ">
+            <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold ">
+              Order Now
+            </div>
+            <div className="">
+              <DoubleBtn theme="blue" text="Contact Us" />
+            </div>
+          </div>
+          <div className=" flex flex-col gap-[12px] w-[250px] ">
+            <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]  ">
+              How we send products?
+            </div>
+            <div className="">
+              <DoubleBtn theme="blue" text="Shipping" />
+            </div>
           </div>
         </div>
       </div>
