b22795a22e11dc4c3dabb20f9cde877c3c9b83f8
UnknownGroup2023 <unknownreact2023@gmail.com>
2025-02-22 16:37:23 +0330

    fetch api(Product Details)

--- src/app/products/[category]/[id]/page.tsx
+++ src/app/products/[category]/[id]/page.tsx
@@ -1,85 +1,121 @@
+"use client";
 import { BackButton1, BackButton2 } from "@/components/BackButton";
-
 import DoubleBtn from "@/components/common/DoubleButton";
 import Rate from "@/components/common/Rate";
 import Slider from "@/components/common/Slider";
+import apiFetcher from "@/core/services/api/fetcher.api";
 import Image from "next/image";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-import React from "react";
+interface Feature {
+  title: string;
+  value: string;
+}
+interface ProductDetails {
+  name: string;
+  feature: Feature[];
+  details: string;
+  rate: number;
+  Packing: string;
+  weight: number;
+}
 
 function page() {
+  const product = useParams();
+
+  const [details, setDetails] = useState<ProductDetails | null>(null);
+
+  const GetData = async () => {
+    try {
+      const data: ProductDetails = await apiFetcher(`/products/${product.id}`);
+
+      setDetails(data);
+    } catch (error) {
+      console.error("Error fetching product details:", error);
+    }
+  };
+
+  useEffect(() => {
+    GetData();
+  }, []);
+
   return (
     <>
       <div className="flex items-center justify-center md:mx-auto ml-[-120px] gap-[80px] md:gap-[40px] mt-[123px] md:mt-[123px] ">
         <BackButton2 />
-
         <div className="w-[45px] h-[19px] text-[16px] leading-[19.2px] text-[#000000] opacity-50 text-center">
           Caviar
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center md:ml-[-150px] mt-[-25px]  justify-center px-6 md:px-12 py-4 gap-[20px] md:gap-x-[103px]">
+      <div className="flex flex-wrap items-center md:ml-[-150px] mt-[-25px] justify-center px-6 md:px-12 py-4 gap-[20px] md:gap-x-[103px]">
         <BackButton1 />
-
-        <div className="text-[40px]  md:text-[48px]  text-[#0D0A0B] text-center md:text-center">
-          Beluga Imperial Top
+        <div className="text-[40px] md:text-[48px] text-[#0D0A0B] text-center md:text-center">
+          {details?.name}
         </div>
       </div>
 
-      <div className="  flex flex-col  md:flex md:flex-wrap justify-center mt-[-20px] items-start  md:items-center mx-auto md:ml-[290px]  w-[600px] gap-[45px]   h-[330px]">
-        <div className=" md:justify-center   hidden md:flex md:flex-col   gap-[24px] w-[160px] ">
-          <div className=" flex flex-col gap-[12px] w-[200px] ">
-            <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold ">
+      <div className="flex flex-col md:flex md:flex-wrap justify-center mt-[-20px] items-start md:items-center mx-auto md:ml-[290px] w-[600px] gap-[45px] h-[330px]">
+        <div className="md:justify-center hidden md:flex md:flex-col gap-[24px] w-[160px] ">
+          <div className="flex flex-col gap-[12px] w-[200px]">
+            <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold">
               Order Now
             </div>
-            <div className="">
+            <div>
               <DoubleBtn theme="blue" text="Contact Us" />
             </div>
           </div>
-          <div className=" flex flex-col gap-[12px] w-[250px] ">
-            <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]  ">
+          <div className="flex flex-col gap-[12px] w-[250px]">
+            <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]">
               How we send products?
             </div>
-            <div className="">
+            <div>
               <DoubleBtn theme="blue" text="Shipping" />
             </div>
           </div>
         </div>
-        <div className="  w-[350px] md:mt-0 md:ml-0 ml-[80px] mt-[220px]  ">
+        <div className="w-[350px] md:mt-0 md:ml-0 ml-[50px] mt-[230px]">
           <Slider />
         </div>
+        <div className="w-[350px]">
+          <div className="md:mt-[0] mt-[62px] md:ml-0 ml-[32px] justify-start flex flex-wrap gap-[145px] md:gap-[48px]">
+            <div className="flex flex-col md:gap-[16px] gap-[8px]">
+              {details?.feature?.map((feature, index) => (
+                <>
+                  <div
+                    key={index}
+                    className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50"
+                  >
+                    {feature.title}
+                  </div>
+                </>
+              ))}
 
-        <div className="  w-[350px]">
-          <div className="  md:mt-[0]  mt-[62px] md:ml-0 ml-[32px]  justify-start  flex  flex-wrap gap-[145px] md:gap-[48px]">
-            <div className=" flex flex-col md:gap-[16px] gap-[8px] ">
-              <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
-                Color
-              </div>
-              <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
-                Size
-              </div>
               <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
                 Rate
               </div>
             </div>
-            <div className=" flex flex-col md:gap-[16px] gap-[8px] ">
+            <div className="flex flex-col md:gap-[16px] gap-[8px]">
+              {details?.feature?.map((feature, index) => (
+                <>
+                  <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
+                    {feature.value}
+                  </div>
+                </>
+              ))}
+
               <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-                Dark Gray
-              </div>
-              <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-                3.3 ≤ mm Up
-              </div>
-              <div className="w-[100px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-                <Rate maxRating={5} />
+                <Rate initialRating={details?.rate} />
               </div>
             </div>
           </div>
-          <div className="text-[16px] mt-[26px] leading-[19.2px] w-full md:ml-0 ml-[32px] h-[57px] text-[#000000] ">
-            A unique Korean flavor with a special sea taste, a silky smooth and
-            velvety texture, and a distinctive salty taste
+          <div className="text-[16px] mt-[26px] leading-[19.2px] w-full md:ml-0 ml-[32px] h-[57px] text-[#000000]">
+            {details?.details}
           </div>
         </div>
       </div>
+
       <div className="flex flex-wrap gap-[76px] md:gap-[86px] md:ml-0 ml-[32px] md:justify-center justify-start items-center mt-[10px]">
         <div className="flex flex-col gap-[8px]">
           <div className="text-[16px] leading-[19.2px] w-[39px] h-[19px] text-[#000000] opacity-50">
@@ -92,29 +128,29 @@ function page() {
 
         <div className="flex flex-col gap-[8px]">
           <div className="w-[49px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            50 g
+            {details?.weight}
           </div>
           <div className="w-[74px] h-[19px] text-[16px] leading-[19.2px] text-[#000000]">
-            Desjardins
+            {details?.Packing}
           </div>
         </div>
+      </div>
 
-        <div className=" md:hidden flex justify-center items-center mt-[112px]  gap-[24px] w-[400px] h-[100px] ">
-          <div className=" flex flex-col gap-[12px] w-[250px] ">
-            <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold ">
-              Order Now
-            </div>
-            <div className="">
-              <DoubleBtn theme="blue" text="Contact Us" />
-            </div>
+      <div className=" md:hidden flex justify-center items-center mt-[202px]  ml-[32px]  gap-[24px] w-[400px] h-[100px] ">
+        <div className=" flex flex-col gap-[12px] w-[250px] ">
+          <div className="w-[105px] h-[24px] text-[20px] leading-[24px] text-[#0D0A0B] font-montrealBold ">
+            Order Now
           </div>
-          <div className=" flex flex-col gap-[12px] w-[250px] ">
-            <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]  ">
-              How we send products?
-            </div>
-            <div className="">
-              <DoubleBtn theme="blue" text="Shipping" />
-            </div>
+          <div className="">
+            <DoubleBtn theme="blue" text="Contact Us" />
+          </div>
+        </div>
+        <div className=" flex flex-col gap-[12px] w-[250px] ">
+          <div className="w-full h-[19px] text-[16px] leading-[19.2px] text-[#0D0A0B]  ">
+            How we send products?
+          </div>
+          <div className="">
+            <DoubleBtn theme="blue" text="Shipping" />
           </div>
         </div>
       </div>
--- src/components/common/Rate/index.tsx
+++ src/components/common/Rate/index.tsx
@@ -3,14 +3,11 @@ import Rating from "@mui/material/Rating";
 import { useState } from "react";
 
 interface RatingProps {
-  maxRating: number;
   initialRating?: number;
 }
 
-const Rate: React.FC<RatingProps> = ({ maxRating, initialRating = 2 }) => {
-  const [value, setValue] = useState<number | null>(initialRating);
-
-  return <Rating name="rating" value={value} max={maxRating} readOnly />;
+const Rate: React.FC<RatingProps> = ({ initialRating }) => {
+  return <Rating name="rating" value={initialRating} max={5} readOnly />;
 };
 
 export default Rate;
